32b846e9997890369c0df9636bac894372f80b88
renshei <100230983@student.burnley.ac.uk>
2026-03-03 15:32:35 +0000

    first attempt

--- station.lua
+++ station.lua
@@ -1,1 +1,23 @@
+local fileName = "pos_data"
 
+print("searching for gps satellites") -- need to make these
+local x, y, z = gps.locate(5)
+
+if x then
+    local file = fs.open(fileName, "w")
+    file.writeLine(x)
+    file.writeLine(y)
+    file.writeLine(z)
+    file.close()
+    print("coords saved successfully")
+    print("x:" .. x .. " Y: " .. y .. " Z: " .. z ..)
+else
+    print("error - couldnt get gps signal")
+
+    if fs.exists(fileName) then
+        local file = fs.open(fileName, "r")
+        x = tonumber(file.readLine())
+        print("using cached x: " .. x)
+        file.close()
+    end
+end
