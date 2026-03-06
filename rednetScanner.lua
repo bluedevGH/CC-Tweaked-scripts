7ff8a31ccc3a1baf8544908c15798822695fa496
renshei <100230983@student.burnley.ac.uk>
2026-03-06 13:39:49 +0000

    comments
    Added comments to clarify device scanning process.

--- rednetScanner.lua
+++ rednetScanner.lua
@@ -1,11 +1,12 @@
 rednet.open("back")
 print("scanning for all local hosts")
-local ids = {rednet.lookup("locateMe")}
+local ids = {rednet.lookup("locateMe")} -- this only scans for devices hosting rednet as "locateMe", cange to your own device's rednet host name
 
 if #ids == 0 then
     print("no devices found")
 else
     for _, id in ipairs(ids) do
-        print("found id:" .. id)
+        print("found id:" .. id) -- this will be the ID of the computer you are looking to directly reference
     end
+
 end
